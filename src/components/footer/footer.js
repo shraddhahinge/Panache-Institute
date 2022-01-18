da10e1c3a1e716d369bfb666ac6415b60188c154
shraddhahinge <shraddhahinge400@gmail.com>
2022-01-18 12:21:43 +0530

    added footer & changed logo in header of mobile drawer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,31 +1,33 @@
 /** @jsx jsx */
-import React from 'react';
-import { jsx, Box, Text, Container } from 'theme-ui';
-import { Link } from 'components/link';
-import Logo from 'components/logo';
-import logoLight from 'assets/logo-light.svg';
+import React from "react";
+import { jsx, Box, Text, Container } from "theme-ui";
+import { Link } from "components/link";
+import Logo from "components/logo";
+import logoLight from "assets/panache-small.png";
 export default function Footer() {
   return (
     <Box as="footer" sx={styles.footer}>
       <Container sx={styles.container}>
         <Box
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: ['column', null, null, null, null, 'row'],
+            display: "flex",
+            alignItems: "center",
+            flexDirection: ["column", null, null, null, null, "row"],
           }}
         >
           <Logo image={logoLight} />
           <Text
             as="p"
             sx={{
-              color: '#ffffff',
-              opacity: '0.7',
-              fontSize: '14px',
-              mt: ['10px', null, null, null, null, '0'],
+              color: "#ffffff",
+              opacity: "0.7",
+              fontSize: "14px",
+              ml: "10px",
+              mt: ["10px", null, null, null, null, "0"],
             }}
           >
-            Copyright by {new Date().getFullYear()} RedQ, Inc
+            {/* Copyright by {new Date().getFullYear()} RedQ, Inc */}
+            PANACHE learning beyond school
           </Text>
         </Box>
         <Box sx={styles.linksWrap}>
@@ -42,38 +44,38 @@ export default function Footer() {
 
 const styles = {
   footer: {
-    py: ['40px', null, null, null, null, '30px', '40px'],
-    backgroundColor: '#020718',
-    position: 'relative',
-    '&::before': {
+    py: ["40px", null, null, null, null, "30px", "40px"],
+    backgroundColor: "#020718",
+    position: "relative",
+    "&::before": {
       content: '""',
-      position: 'absolute',
+      position: "absolute",
       top: 0,
       left: 0,
       right: 0,
       bottom: 0,
-      backgroundColor: '#ffffff',
-      opacity: '0.05',
+      backgroundColor: "#ffffff",
+      opacity: "0.05",
     },
   },
   container: {
-    display: 'flex',
-    flexDirection: ['column', null, null, null, null, 'row'],
-    justifyContent: ['center', null, null, null, null, 'space-between'],
-    alignItems: 'center',
-    position: 'relative',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexDirection: ["column", null, null, null, null, "row"],
+    justifyContent: ["center", null, null, null, null, "space-between"],
+    alignItems: "center",
+    position: "relative",
+    flexWrap: "wrap",
   },
   linksWrap: {
-    mt: ['15px', null, null, null, null, '0'],
-    display: 'flex',
-    flexWrap: 'wrap',
+    mt: ["15px", null, null, null, null, "0"],
+    display: "flex",
+    flexWrap: "wrap",
     a: {
-      fontSize: ['14px', null, null, null, '16px'],
-      color: '#ffffff',
-      transition: 'all 500ms ease',
-      '&:hover': { opacity: 0.7 },
+      fontSize: ["14px", null, null, null, "16px"],
+      color: "#ffffff",
+      transition: "all 500ms ease",
+      "&:hover": { opacity: 0.7 },
     },
-    'a+a': { ml: ['15px', null, null, null, '35px'] },
+    "a+a": { ml: ["15px", null, null, null, "35px"] },
   },
 };
